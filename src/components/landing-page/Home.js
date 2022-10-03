4e6581993df2263ccfc87525d739dfaa466f45ae
sean-code <john.nganga@student.moringaschool.com>
2022-10-03 20:34:29 +0300

    Add the Home Contents for the Landing page

--- src/components/landing-page/Home.js
+++ src/components/landing-page/Home.js
@@ -2,8 +2,15 @@ import './Home.css'
 
 function Home(){
     return(
-        <div className='container'>
-            <h1>Movie Search+<sub>developed by Sean</sub></h1>
+        <div className='main'>
+            <div className='contents'>
+                <h1>Movie Search+<sub>developed by Sean</sub></h1>
+                <p>
+                    Catch The Latest Movies In High Quality. 
+                    <br />Plus With No ADS! Enjoy!!<br /> 
+                </p>
+            </div>
+
         </div>
     )
 }
