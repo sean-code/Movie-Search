67b3f9286de0cbc3f2a030e1e93d298a403d26d5
sean-code <john.nganga@student.moringaschool.com>
2022-10-04 21:25:52 +0300

    Create a MovieList Component to pass our props and map through our data

---
+++ src/components/search/MovieList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MovieList = (props) =>{
+    return(
+        <>
+            {props.movies.map((movie, index)=> <div>
+                <img src={movie.Poster} alt='img'></img>
+            </div>)}
+        </>
+    )
+}
+
+
+export default MovieList;
