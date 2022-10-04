b7b225f60045d3bee3d95a00054591b952a944c5
sean-code <john.nganga@student.moringaschool.com>
2022-10-05 00:31:15 +0300

    Create Signin form and style it using custom CSS

--- src/components/authentication/Signin.js
+++ src/components/authentication/Signin.js
@@ -1,7 +1,27 @@
+import './Signin.css'
 
 function Signin(){
     return(
-        <div>Signin</div>
+        <div className='login-box'>
+            <h2>Sign In</h2>
+            <form>
+                <div className="user-box">
+                    <input type="text" name="" required="" />
+                    <label>Username</label>
+                </div>
+                <div className="user-box">
+                    <input type="password" name="" required="" />
+                    <label>Password</label>
+                </div>
+                <a href="#">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                    Sign In
+                </a>
+            </form> 
+        </div>
     )
 }
 
