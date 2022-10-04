70c12a3df7854ad29f75a4f7d88a6e1da77f2a8a
sean-code <john.nganga@student.moringaschool.com>
2022-10-04 08:28:05 +0300

    Import the Navbar from its different folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './navbar/Navbar';
 import Home from './landing-page/Home';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Home/>
     </div>
   );
