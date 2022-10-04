dce9b4345a4d6bb7b11e28bb8d3570548fc55ffc
sean-code <john.nganga@student.moringaschool.com>
2022-10-04 12:29:23 +0300

    set up animation for the landing page image

--- src/components/landing-page/Home.js
+++ src/components/landing-page/Home.js
@@ -1,16 +1,17 @@
 import './Home.css'
 
+
 function Home(){
     return(
-        <div className='main'>
+        <div className='main' id='rotate'>
+            <img src='https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bW92aWUlMjBwb3N0ZXJzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' />
             <div className='contents'>
-                <h1>Movie Search+<sub>developed by Sean</sub></h1>
+                    <h1>Movie Search+<sub>developed by Sean</sub></h1>
                 <p>
                     Catch The Latest Movies In High Quality. 
                     <br />Plus With No ADS! Enjoy!!<br /> 
                 </p>
             </div>
-
         </div>
     )
 }
