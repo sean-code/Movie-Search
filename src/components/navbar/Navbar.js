3495596e8487cdd9e206b8a0b12498ad2fd99f36
sean-code <john.nganga@student.moringaschool.com>
2022-10-04 14:13:19 +0300

    Add the NavLink from the react-router-dom into the Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,26 +4,28 @@ import { NavLink } from 'react-router-dom';
 function Navbar(){
     return(
         <nav className="main-navigation">
+        <div>
             <ul className="nav-list">
-                <li className="nav-list-item">
-                    <a to='/' className="nav-link">Home</a>
-                </li>
-                <li className="nav-list-item">
-                    <a to='/search' className="nav-link">Search</a>
-                </li>
-                <li className="nav-list-item">
-                    <a to='/about' className="nav-link">About</a>
-                </li>
-                <li className="nav-list-item">
-                    <a to='/feedback' className="nav-link">Feedback</a>
-                </li>
-                <li className="nav-list-item">
-                    <a to='contact' className="nav-link">Contact</a>
-                </li>
-                <li className="nav-list-item">
-                    <a to='/authentication' className="nav-link">Log In</a>
-                </li>
-            </ul>
+                    <li className="nav-list-item">
+                        <NavLink to='/' className="nav-link">Home</NavLink>
+                    </li>
+                    <li className="nav-list-item">
+                        <NavLink to='/search' className="nav-link">Search</NavLink>
+                    </li>
+                    <li className="nav-list-item">
+                        <NavLink to='/about' className="nav-link">About</NavLink>
+                    </li>
+                    <li className="nav-list-item">
+                        <NavLink to='/feedback' className="nav-link">Feedback</NavLink>
+                    </li>
+                    <li className="nav-list-item">
+                        <NavLink to='/contact' className="nav-link">Contact</NavLink>
+                    </li>
+                    <li className="nav-list-item">
+                        <NavLink to='/authentication' className="nav-link">Sign In</NavLink>
+                    </li>
+                </ul>
+        </div>
         </nav>
     )
 }
