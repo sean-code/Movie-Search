de5edadaada745f17d6471f0f3135877400a8a6c
sean-code <john.nganga@student.moringaschool.com>
2022-10-06 00:48:55 +0300

    Create a feedback form and style it using custom CSS

--- src/components/feedback/Feedback.js
+++ src/components/feedback/Feedback.js
@@ -1,8 +1,27 @@
-
+import './Feedback.css'
 
 function Feedback(){
     return(
-        <div>Feedback</div>
+    <div className="container"> 
+        <form id="contact" action="" method="post">
+            <h3>Leave Me Your Feedback</h3>
+        <fieldset>
+            <input placeholder="Your name" type="text" tabindex="1" required autofocus />
+        </fieldset>
+        <fieldset>
+            <input placeholder="Your Email Address" type="email" tabindex="2" required />
+        </fieldset>
+        <fieldset>
+            <input placeholder="Your Phone Number (optional)" type="tel" tabindex="3" required />
+        </fieldset>
+        <fieldset>
+            <textarea placeholder="Type your message here...." tabindex="5" required></textarea>
+        </fieldset>
+        <fieldset>
+            <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+        </fieldset>
+        </form>
+    </div>
     )
 }
 
