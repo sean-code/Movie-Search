26326e7939c50e79844de1b1ab8bc7901adb2cdf
sean-code <john.nganga@student.moringaschool.com>
2022-10-05 12:08:46 +0300

    Remove the hard coded data arrayed objects, add a useState hook with an empty string, and then create a dynamic component for displaying our heading for search results

---
+++ src/components/search/MovieListHeading.js
@@ -0,0 +1,12 @@
+
+
+function MovieListHeading (props) {
+    return(
+        <div className="col">
+            <h2>{props.heading}</h2>
+
+        </div>
+    )
+}
+
+export default MovieListHeading;
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,47 +1,11 @@
 import React, {useState, useEffect} from "react";
 import './Search.css';
-
 import MovieList from "./MovieList";
+import MovieListHeading from "./MovieListHeading";
 
 function Search(){
-    const[movies, setMovies] = useState([
-
-        {
-            "Title": "Dune",
-            "Year": "2021",
-            "imdbID": "tt1160419",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Eternals",
-            "Year": "2021",
-            "imdbID": "tt9032400",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BY2Y1ODBhYTItYmJiZi00NjU2LWI2NjktNTcwM2U2NGQ2ZTNiXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg"
-        },
-        {
-            "Title": "The Batman",
-            "Year": "2022",
-            "imdbID": "tt1877830",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Bullet Train",
-            "Year": "2022",
-            "imdbID": "tt12593682",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMDU2ZmM2OTYtNzIxYy00NjM5LTliNGQtN2JmOWQzYTBmZWUzXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg"
-        },
-        {
-            "Title": "The Woman King",
-            "Year": "2022",
-            "imdbID": "tt8093700",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BY2I4MDIwYWUtOWMxNC00ZTIzLWE3OGYtOWUyMmIwZGE2NjU4XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_SX300.jpg"
-        }
-    ]);
+    const[movies, setMovies] = useState([]);
+    const [searchMovie, setSearchMovie] = useState('');
 
     const getMovieRequest = async() =>{
         const url = 'http://www.omdbapi.com/?s=Batman&apikey=6f97e522';
@@ -50,7 +14,7 @@ function Search(){
         const responseJson = await response.json();
 
         // console.log(responseJson);
-        setMovies(responseJson.Search);
+          setMovies(responseJson.Search);
         
     }
     useEffect(()=> {
@@ -59,6 +23,10 @@ function Search(){
 
     return(
         <div className="container-fluid movie-app">
+            <div className="row">
+                <MovieListHeading heading='Search Result'/>
+
+            </div>
             <div className="row">
                 <MovieList movies={movies}/>
             </div>
