be7d8f0b67db642ae4a0490126d5c2684edb9521
sean-code <john.nganga@student.moringaschool.com>
2022-10-06 14:29:16 +0300

    Validate Form

--- src/components/feedback/Feedback.js
+++ src/components/feedback/Feedback.js
@@ -1,5 +1,5 @@
-import { Axios } from 'axios';
-import React, {useState} from 'react';
+
+import React, {useRef, useState} from 'react';
 import './Feedback.css';
 import axios from 'axios';
 
@@ -12,6 +12,14 @@ function Feedback(){
         message: ""
     })
 
+    const[validated, setValidated] = useState(); // To validate the form
+    const formRef = useRef(null);
+
+    const handleReset = () => {
+        formRef.current.reset();
+        setValidated(false);
+      };
+
     function handle(event){
         const newdata = {...data}
         newdata[event.target.id] = event.target.value
@@ -20,7 +28,7 @@ function Feedback(){
     }
 
     const submit = (event)=>{
-        event.preventDefault();
+        event.preventDefault(); //prevent page refresh
         axios.post(url, {
             name: data.name,
             email: data.email,
@@ -30,27 +38,32 @@ function Feedback(){
         .then(response=>{
             console.log(response.data)
         })
+
+        setValidated(true);
+        handleReset();
+
     }
+
 
     return(
     <div className="container"> 
-        <form id="contact" onSubmit={(event)=> submit(event)}>
+        <form id="contact" formRef={formRef}  validated={validated}  onSubmit={(event)=> submit(event)}>
             <h3>Leave Me Your Feedback</h3>
-        <fieldset>
-            <input onChange ={(event)=>handle(event)} id="name" value={data.name} placeholder="Your name" type="text" tabindex="1" required autofocus />
-        </fieldset>
-        <fieldset>
-            <input onChange ={(event)=>handle(event)} id="email" value={data.email}placeholder="Your Email Address" type="email" tabindex="2" required />
-        </fieldset>
-        <fieldset>
-            <input onChange ={(event)=>handle(event)} id="phonenumber" value={data.phonenumber} placeholder="Your Phone Number (optional)" type="tel" tabindex="3" required />
-        </fieldset>
-        <fieldset>
-            <textarea onChange ={(event)=>handle(event)} id="message" value={data.message} placeholder="Type your message here...." tabindex="5" required></textarea>
-        </fieldset>
-        <fieldset>
-            <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-        </fieldset>
+            <fieldset>
+                <input onChange ={(event)=>handle(event)} id="name" value={data.name} placeholder="Your name" type="text" tabindex="1" required autofocus />
+            </fieldset>
+            <fieldset>
+                <input onChange ={(event)=>handle(event)} id="email" value={data.email}placeholder="Your Email Address" type="email" tabindex="2" required />
+            </fieldset>
+            <fieldset>
+                <input onChange ={(event)=>handle(event)} id="phonenumber" value={data.phonenumber} placeholder="Your Phone Number (optional)" type="tel" tabindex="3" required />
+            </fieldset>
+            <fieldset>
+                <textarea onChange ={(event)=>handle(event)} id="message" value={data.message} placeholder="Type your message here...." tabindex="5" required></textarea>
+            </fieldset>
+            <fieldset>
+                <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+            </fieldset>
         </form>
     </div>
     )
