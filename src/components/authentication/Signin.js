b94ea5bd1d7201e4091a03e2ac65a06f5f717467
sean-code <john.nganga@student.moringaschool.com>
2022-10-06 18:34:21 +0300

    Validate form for the authentication component

--- src/components/authentication/Signin.js
+++ src/components/authentication/Signin.js
@@ -1,17 +1,35 @@
 import './Signin.css'
-
+import {useState} from 'react';
 function Signin(){
+    const [userName, setUserName] = useState('');
+    const [thePassword, setThePassword] = useState('');
+    const [error, setError] = useState(false);
+
+function handleSubmit(event){
+    event.preventDefault();
+    if(userName.length == 0 || thePassword == 0){
+        setError(true)
+    }
+    if (userName&&thePassword)
+    {
+
+    console.log("Username: ",userName, "\nThe Password: ", thePassword)
+    }
+}
+
     return(
         <div className='login-box'>
             <h2>Sign In</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="user-box">
-                    <input type="text" name="" required="" />
-                    <label>Username</label>
+                    <input onChange ={event=> setUserName(event.target.value)} placeholder='Enter Username' type="text" name="" required="" />
+                    {error&&userName.length<=0?
+                    <label>Username</label>:""}
                 </div>
                 <div className="user-box">
-                    <input type="password" name="" required="" />
-                    <label>Password</label>
+                    <input onChange ={event=> setThePassword(event.target.value)} placeholder='Enter Username' type="password" name="" required="" />
+                    {error&&thePassword.length<=0?
+                    <label>Password</label>: ""}
                 </div>
                 <a href="#">
                 <span></span>
