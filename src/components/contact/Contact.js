df8894771ab87238ebe408215197cc6d503cd1f0
sean-code <john.nganga@student.moringaschool.com>
2022-10-06 14:56:28 +0300

    Repair the failing anchored links on Contact me page

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -4,13 +4,15 @@ function Contact(){
     return(
         <div className="container">
             <div className="card">
+            <a href='https://www.twitter.com/nganga_sea' target='_blank'>
                 <h3 className="title">Twitter</h3>
+            </a>
             <div className="bar">
                 <div className="emptybar"></div>
                 <div className="filledbar"></div>
             </div>
             <div className="circle">
-                <a href='https://www.twitter.com/nganga_sea'>
+                <a href='https://www.twitter.com/nganga_sea' target='_blank'>
                     @nganga_sea
                 </a>
                 <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
@@ -19,13 +21,15 @@ function Contact(){
             </div>
         </div>
         <div className="card">
-            <h3 className="title">GitHub</h3>
+            <a href='https://github.com/sean-code' target='_blank'>
+                <h3 className="title">GitHub</h3>
+            </a>
             <div className="bar">
             <div className="emptybar"></div>
             <div className="filledbar"></div>
             </div>
             <div className="circle">
-                <a href='https://github.com/sean-code'>
+                <a href='https://github.com/sean-code' target='_blank'>
                     @Sean-Code
                 </a>
                 <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
@@ -34,13 +38,15 @@ function Contact(){
             </div>
         </div>
         <div className="card">
-            <h3 className="title">Instagram</h3>
+            <a href='https://www.instagram.com/its_ngangasean' target='_blank'>
+                <h3 className="title">Instagram</h3>
+            </a>
             <div className="bar">
             <div className="emptybar"></div>
             <div className="filledbar"></div>
             </div>
             <div className="circle">
-                <a href='https://www.instagram.com/sean-code'>
+                <a href='https://www.instagram.com/its_ngangasean' target='_blank'>
                     @its_ngangasean
                 </a>
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
@@ -49,13 +55,15 @@ function Contact(){
             </div>
         </div>
         <div className="card">
-            <h3 className="title">Email</h3>
+            <a href="mailto:email@example.com" target='_blank'>
+                <h3 className="title">Email</h3>
+            </a>
             <div className="bar">
                 <div className="emptybar"></div>
                 <div className="filledbar"></div>
             </div>
             <div className="circle">
-            <a href="mailto:email@example.com">
+            <a href="mailto:email@example.com" target='_blank'>
                 ngangasean@yahoo.com
             </a>
                 <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
