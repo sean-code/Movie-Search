ab82aecfcbce4db68f683a55e3c4ed6f76e1a98f
sean-code <john.nganga@student.moringaschool.com>
2022-10-06 12:41:14 +0300

    Add event listener for onCahnge in the feedback form

--- src/components/feedback/Feedback.js
+++ src/components/feedback/Feedback.js
@@ -10,21 +10,25 @@ function Feedback(){
         message: ""
     })
 
+    function handle(event){
+        
+    }
+
     return(
     <div className="container"> 
         <form id="contact" action="" method="post">
             <h3>Leave Me Your Feedback</h3>
         <fieldset>
-            <input placeholder="Your name" type="text" tabindex="1" required autofocus />
+            <input onChange ={(event)=>handle(event)} id="name" value={data.name} placeholder="Your name" type="text" tabindex="1" required autofocus />
         </fieldset>
         <fieldset>
-            <input placeholder="Your Email Address" type="email" tabindex="2" required />
+            <input onChange ={(event)=>handle(event)} id="email" value={data.email}placeholder="Your Email Address" type="email" tabindex="2" required />
         </fieldset>
         <fieldset>
-            <input placeholder="Your Phone Number (optional)" type="tel" tabindex="3" required />
+            <input onChange ={(event)=>handle(event)} id="phonenumber" value={data.phonenumber} placeholder="Your Phone Number (optional)" type="tel" tabindex="3" required />
         </fieldset>
         <fieldset>
-            <textarea placeholder="Type your message here...." tabindex="5" required></textarea>
+            <textarea onChange ={(event)=>handle(event)} id="message" value={data.message} placeholder="Type your message here...." tabindex="5" required></textarea>
         </fieldset>
         <fieldset>
             <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
